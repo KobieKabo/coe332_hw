fa95f3206a397e1e98c529076a2c10d2ce6e78ef
Jakob Long <Jakob.long@utexas.edu>
2023-03-28 05:39:55 +0000

    as done as im willing

--- homework06/gene_api.py
+++ homework06/gene_api.py
@@ -1,80 +1,100 @@
-from flask import Flask, request
 import json
-import requests
 import redis
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
-def get_redis_client():
-    return redis.Redis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
+def get_redis():
+    """
+    Constructs the redis client that's used to store data.
 
-rd = get_redis_client
+    Args: none
 
-def data_status():
-    global data_state
-    if data_state == False:
-        return 'Data has been deleted. Please re-post the data before using the application.\n'
+    Returns: A redis client
+    """
+    return redis.Redis(host = 'redis-db', port=6379, db=0, decode_responses = True)
 
+rd = get_redis()
 
-@app.route('/data', methods = ['GET','DELETE','POST'])
-def data_handler():
-    
-    if request.method == 'DELTE':
+@app.route('/data', methods = ['GET','POST','DELETE'])
+def handle_data():
+    """
+    Method that handles the dataset we're working with.
+
+    Args: None
+
+    Curl Methods:
+        Get: Returns data from redis db
+        Post: posts data into the redis db
+        Delete: deletes the data from the redis db
+
+    Returns:
+        Get: A list of the dataset
+        Post: A string declaring the data as posted
+        Delete: A string declaring the data as deleted
+    """
+    global rd
+
+    if request.method == 'DELETE':
         rd.flushdb()
-        global data_state 
-        data_state = False
         return 'Data has been deleted.\n'
+    
+    elif request.method == 'POST':
+        r = requests.get(url = 'https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json')
+        gene_data = r.json()
+
+        for item in gene_data['response']['docs']:
+            key = f'{item[hgnc_id]}'
+            rd.hset(key, mapping = item)
+        return 'Data has been posted.\n'
 
     elif request.method == 'GET':
-        if data_state == False:
-            return data_status()
-
         data_list = []
-        for key in rd.keys():
-            data_list.append(rd.hgetall(key))
+        for item in rd.keys():
+            data_list.append(rd.hgetall(item))
         return data_list
-
-    elif request.method == 'POST':
-        url = "https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json"
-        r = requests.get(url)
-        data = r.json()
-
-        for item in data['gene_data']:
-            key = f'{item["name"]}:{item["id"]}'
-            rd.hset(key,mapping=item)
-
-        data_state = True
-        return 'Data has been posted to the Redis database.\n'
     
     else:
-        return 'Invalid method has been provided. Please use either POST, DELETE, or GET. Refer to README for assistance.\n'
+        return 'An incorrect method was provided. Please use POST, GET or DELETE.\n'
 
 @app.route('/genes', methods = ['GET'])
-def get_all_gene_ids() -> list:
-    try:
-        if data_state == False:
-            return data_status()
+def get_all_hgncID() -> list:
+    """
+    Creates a json formated list of all the hgnc gene IDs.
 
-        gene_keys = rd.keys()
-        gene_data = []
+    Args: 
+    None
 
-        for gene_keys in gene_keys:
-            hgnc_id = rd.hget(key, 'hgnc_id')
-            if hgnc_id:
-                gene_data.append(hgnc_id)
-        return gene_data
+    Returns:
+        List of the hgnc_id's within the dataset    
+    """
 
-@app.route('/genes/<string:hgnc_id>', methods = ['GET'])
-def get_gene_id_data(hgnc_id):
-    try:
-        if data_state == False:
-            return data_status()
-        gene_data = {}
-        gene_data = rd.hgetall(hgnc_id)
+    geneIDs = []
 
-        if not gene_data:
-            return f'{hgnc_id}: is not a valid gene id.\n'
-        return gene_data
+    for key in rd.keys():
+        geneIDs.append(key)
+    return geneIDs
+
+@app.route('/genes.<hgnc_id>', methods = ['GET'])
+def get_hgnc_data(hgnc_id) -> dict:
+    """
+    Returns all of the information associated with a specific hgnc_id
+
+    Args: None
+
+    Returns:
+        A dictionary of the data associated with the given hgnc_id
+    """
+    if len(rd.keys()) == 0:
+        return 'Database is empty. please re-post the data.\n'
+    
+    for key in rd.keys():
+        if str(key) == str(hgnc_id):
+            return json.loads(rd.get(key))
+
+    return 'The given hgnc_id did not have a match. Please try another.\n'
+
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
